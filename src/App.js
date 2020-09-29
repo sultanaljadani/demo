d12daa4ad660a9d329428dd2330378e8171fbda4
Sultan <isultaninho@gmail.com>
2020-09-29 09:33:14 +0300

    organize  header component and paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/Header'
+import Header from './components/Header/Header'
 import Products from './components/Products'
 import About from './components/About'
 import Contact from './components/Contact'
