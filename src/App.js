8307adf3102b52ef9b173647340daed73848c572
Sultan <isultaninho@gmail.com>
2020-10-12 14:10:25 +0300

    custaming app.js file into MasrterLayout.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import React from 'react';
-import Header from './components/Header/Header'
-import Products from './components/Products/Products'
-import About from './components/About/About'
-import Contact from './components/Contact/Contact'
-import Cart from './components/Cart/Cart'
-import Footer from './components/Footer/Footer'
-import ProductDetiles from './components/ProductDetiles/ProductDetiles'
+
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {DataProvider} from './components/DataProvider'
+import MasterLayout from './components/master-layout/MasterLayout'
+import './App.less';
 
 function App() {
   return (
     <DataProvider>
-      <div>
-      <Router>
-          <Header />
-          <section>
-              <Route path="/products" component={Products} />
-              <Route path="/productDetiles/:id" component={ProductDetiles} />
-              <Route path="/about" component={About} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/cart" component={Cart} />
-          </section>
-          </Router>
-      </div>
-      <Footer />
+        <MasterLayout/>
     </DataProvider>
-    
   );
 }
 
