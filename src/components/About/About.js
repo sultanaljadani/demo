15b183be28bb32834754ff8b032ac9a920e412b2
Sultan <isultaninho@gmail.com>
2020-10-12 14:12:16 +0300

    adding antd components into About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import React , { useContext } from 'react'
 import {DataContext} from '../DataProvider'
-import {Card, Avatar,Col, Row } from 'antd'
+import {Card, Col, Row , Avatar, Tag, PageHeader} from 'antd'
 import './About.css'
 import { CodeSandboxOutlined, GithubOutlined, TwitterCircleFilled } from '@ant-design/icons';
 export default function About() {
@@ -11,34 +11,39 @@ export default function About() {
         textAlign: 'center',
         padding: '30px',
         margin: '10px',
-        borderRadius: '10px',
-      };
+        borderRadius: '10px', };
     return (
         <>
-        <div className="about">
-            <h2 className="title-page">Team Awesome</h2>
-            {
-            about.map(about => (
-                <Row align="middle" justify="space-around" key={about.__id} gutter="vertical">
-                    <Col span={8}>
-                        <Card key={about.__id}
-                            bordered={true}
-                            style={gridStyle}
-                            actions={[
-                            <CodeSandboxOutlined key="codesandbox" href=''/>,
-                            <GithubOutlined key="github" />,
-                            <TwitterCircleFilled key="twitter" title={about.twitter_account}  />,
-                            ]}>
-                            <Meta
-                            avatar={<Avatar size={64} src={about.image[0]} />}
-                            title={about.name}
-                            description={about.position}/>
-                        </Card>
-                    </Col>
-                </Row>
-                ))
-            }
-        </div>
+            <div className="about">
+            <PageHeader
+            className="site-page-header"
+            onBack={() => window.history.back()}
+            title="Back"
+            subTitle="Who are we ? "
+            tags={<Tag color="#41a098">About us</Tag>} />
+                <h2 className="title-page">Team Awesome</h2>
+                    <Row align="middle" justify="space-around"  gutter="horizt">
+                        {
+                            about.map(about => (
+                                <Col span={6} key={about.__id}>
+                                    <Card key={about.__id}
+                                        bordered={true}
+                                        style={gridStyle}
+                                        actions={[
+                                            <CodeSandboxOutlined key="codesandbox" href=''/>,
+                                            <GithubOutlined key="github" />,
+                                            <TwitterCircleFilled key="twitter" title={about.twitter_account}  />,
+                                            ]}>
+                                            <Meta
+                                                avatar={<Avatar size={64} src={about.image[0]} />}
+                                                title={about.name}
+                                                description={about.position}/>
+                                    </Card>
+                                </Col>
+                                ))
+                        }
+                    </Row>
+            </div>
         </>
     )
 }
