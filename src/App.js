de4bdd9c21907595d1a7524502e2f8005b73dd5f
Sultan <isultaninho@gmail.com>
2020-10-18 13:35:19 +0300

    adding localization

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {DataProvider} from './components/DataProvider'
 import MasterLayout from './components/master-layout/MasterLayout'
 import './App.less';
+import 'moment/locale/zh-cn';
+import locale from 'antd/es/locale/zh_CN';
 function App() {
   return (
-    <DataProvider>
+    <DataProvider locale={locale}>
         <MasterLayout/>
     </DataProvider>
   );
