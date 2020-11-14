30a1e270b88ae0d92bdeeb5eaa691d137f678c14
sultanaljadani <32942031+sultanaljadani@users.noreply.github.com>
2020-11-14 16:03:41 +0300

    adding reducer types

---
+++ src/redux/Shopping/shopping-types.js
@@ -0,0 +1,4 @@
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const ADJUST_QTY = "ADJUST_QTY";
+export const LODE_CURRENT_ITEM = "LODE_CURRENT_ITEM";
