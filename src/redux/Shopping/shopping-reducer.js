b5d938f59ac857bad0c989feeeac93017f0400cf
sultanaljadani <32942031+sultanaljadani@users.noreply.github.com>
2020-12-01 14:31:53 +0300

    made some changes on reducer shopping

--- src/redux/Shopping/shopping-reducer.js
+++ src/redux/Shopping/shopping-reducer.js
@@ -3,74 +3,82 @@ import * as actionTypes from './shopping-types';
 export const INITIAL_STATE = {
     products: [
         {
-            "_id":"1",
-             "title": "Apple iPhone 11 Pro Max",
-             "images": ["https://media.extra.com/i/aurora/100091664_100_02?w=400&h=400&img404=missing_product&v=false"],
-             "description": "4K video at 60 fps on every camera. Ultra Wide camera for four times more scene. Meet the first triple camera.",
-             "price": 899,
-             "count": 1
+             id:"1",
+             title: "Apple iPhone 11 Pro Max",
+             description: "4K video at 60 fps on every camera. Ultra Wide camera for four times more scene. Meet the first triple camera.",
+             images: ["https://media.extra.com/i/aurora/100091664_100_02?w=400&h=400&img404=missing_product&v=false"],
+             
+             price: 899,
+             count: 1
          },
          {
-             "_id": "2",
-             "title": "Apple iPhone ES",
-             "images": ["https://media.extra.com/i/aurora/100150345_100_04?w=400&h=400&img404=missing_product&v=false"],
-             "description": "Brilliant 4.7˝ Retina HD display. Speed and security of Touch ID. Water resistant to 1 meter for 30 minutes.",
-             "price": 759,
-             "count": 1
+             id: "2",
+             title: "Apple iPhone ES",
+             description: "Brilliant 4.7˝ Retina HD display. Speed and security of Touch ID. Water resistant to 1 meter for 30 minutes.",
+             images: ["https://media.extra.com/i/aurora/100150345_100_04?w=400&h=400&img404=missing_product&v=false"],
+             
+             price: 759,
+             count: 1
 
          },
          {
-             "_id": "3",
-             "title": "Apple iPhone X",
-             "images": ["https://media.extra.com/i/aurora/100038344_100_01?w=400&h=400&img404=missing_product&v=false"],
-             "description": "The custom OLED displays on iPhone XS deliver the most accurate color in the industry, HDR, and true blacks.",
-             "price": 699,
-             "count": 1
+             id: "3",
+             title: "Apple iPhone X",
+             description: "The custom OLED displays on iPhone XS deliver the most accurate color in the industry, HDR, and true blacks.",
+             images: ["https://media.extra.com/i/aurora/100038344_100_01?w=400&h=400&img404=missing_product&v=false"],
+             
+             price: 699,
+             count: 1
 
          },
          {
-             "_id": "4",
-             "title": "Apple iPhone 8",
-             "images": ["https://media.extra.com/i/aurora/100093944_100_02?w=400&h=400&img404=missing_product&v=false"],
-             "description": "iPhone 8 introduces an all-new glass design. The world’s most popular camera, now even better.",
-             "price": 389,
-             "count": 1
+             id: "4",
+             title: "Apple iPhone 8",
+             description: "iPhone 8 introduces an all-new glass design. The world’s most popular camera, now even better.",
+             images: ["https://media.extra.com/i/aurora/100093944_100_02?w=400&h=400&img404=missing_product&v=false"],
+             
+             price: 389,
+             count: 1
 
          },
          {
-             "_id": "5",
-             "title": "Apple iPhone 7",
-             "images": ["https://media.extra.com/i/aurora/00192402_100_02?w=400&h=400&img404=missing_product&v=false"],
-             "description": "With its entire enclosure reengineered, iPhone 7 is the very first water-resistant iPhone, powerful enough to be called Pro",
-             "price": 349,
-             "count": 1
+             id: "5",
+             title: "Apple iPhone 7",
+             description: "With its entire enclosure reengineered, iPhone 7 is the very first water-resistant iPhone, powerful enough to be called Pro",
+             images: ["https://media.extra.com/i/aurora/00192402_100_02?w=400&h=400&img404=missing_product&v=false"],
+             
+             price: 349,
+             count: 1
 
          },
          {
-             "_id": "6",
-             "title": "Apple iPhone 6s",
-             "images": ["https://media.extra.com/i/aurora/00193195_100_02?w=400&h=400&img404=missing_product&v=false"],
-             "description": "iPhone 6s is powered by the custom-designed 64-bit A9 chip. It delivers performance once found only in desktop computers.",
-             "price": 299,
-             "count": 1
+             id: "6",
+             title: "Apple iPhone 6s",
+             description: "iPhone 6s is powered by the custom-designed 64-bit A9 chip. It delivers performance once found only in desktop computers.",
+             images: ["https://media.extra.com/i/aurora/00193195_100_02?w=400&h=400&img404=missing_product&v=false"],
+             
+             price: 299,
+             count: 1
 
          },
          {
-            "_id": "7",
-            "title": "Apple iPhone 5s",
-            "images": ["https://media.extra.com/i/aurora/00193195_100_02?w=400&h=400&img404=missing_product&v=false"],
-            "description": "iPhone 5s is powered by the custom-designed 64-bit A9 chip. It delivers performance once found only in desktop computers.",
-            "price": 199,
-            "count": 1
+            id: "7",
+            title: "Apple iPhone 5s",
+            description: "iPhone 5s is powered by the custom-designed 64-bit A9 chip. It delivers performance once found only in desktop computers.",
+            images: ["https://media.extra.com/i/aurora/00193195_100_02?w=400&h=400&img404=missing_product&v=false"],
+            
+            price: 199,
+            count: 1
 
         },
         {
-            "_id": "8",
-            "title": "Apple iPhone 4s",
-            "images": ["https://media.extra.com/i/aurora/00193195_100_02?w=400&h=400&img404=missing_product&v=false"],
-            "description": "iPhone 4s is powered by the custom-designed 64-bit A9 chip. It delivers performance once found only in desktop computers.",
-            "price": 99,
-            "count": 1
+            id: "8",
+            title: "Apple iPhone 4s",
+            description: "iPhone 4s is powered by the custom-designed 64-bit A9 chip. It delivers performance once found only in desktop computers.",
+            images: ["https://media.extra.com/i/aurora/00193195_100_02?w=400&h=400&img404=missing_product&v=false"],
+            
+            price: 99,
+            count: 1
 
         }
     ], //{id, title, des, price, img... etc}
@@ -107,13 +115,13 @@ export const INITIAL_STATE = {
         "code_account": "adulsal"
       }
     ],
+    currentItem: null,
     get about() {
       return this._about;
     },
     set about(value) {
       this._about = value;
     },
-    currentItem: null,
 };
 const shopReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
